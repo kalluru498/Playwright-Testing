487a1d7c342df7200ccf54a357b7cf508f57f176
kalluru99 <kallurunaveen498@gmail.com>
2026-01-28 19:08:34 -0500

    Initial commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Get started' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});
+
+
+
+test('test demo', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await page.getByRole('banner').click();
+  await page.getByText('PlaywrightDocsAPINode.jsNode.').click();
+  await page.getByRole('heading', { name: 'Playwright enables reliable' }).click();
+  await page.getByText('Get startedStar81k+').click();
+  await page.getByRole('link', { name: 'API' }).click();
+  await page.getByRole('heading', { name: 'Playwright Library' }).click();
+});
